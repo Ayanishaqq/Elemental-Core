6f20106346951978794110dff23ab0f6bd531908
Ayanishaqq <ayanishap.gaming@gmail.com>
2025-07-24 12:49:57 +0500

    Update CoreType.java

--- src/main/java/com/elementalcores/CoreType.java
+++ src/main/java/com/elementalcores/CoreType.java
@@ -8,11 +8,11 @@ import org.bukkit.potion.PotionEffectType;
 
 public enum CoreType {
     EARTH("Earth", "#8B5A2B", 1001, Particle.BLOCK, Material.STONE, Sound.BLOCK_STONE_PLACE, PotionEffectType.RESISTANCE, PotionEffectType.HASTE),
-    WATER("Water", "#1E90FF", 1002, Particle.WATER_SPLASH, Material.WATER_BUCKET, Sound.ENTITY_PLAYER_SPLASH, PotionEffectType.WATER_BREATHING, PotionEffectType.DOLPHINS_GRACE),
+    WATER("Water", "#1E90FF", 1002, Particle.SPLASH, Material.WATER_BUCKET, Sound.ENTITY_PLAYER_SPLASH, PotionEffectType.WATER_BREATHING, PotionEffectType.DOLPHINS_GRACE),
     FIRE("Fire", "#FF4500", 1003, Particle.FLAME, Material.LAVA_BUCKET, Sound.BLOCK_FIRE_AMBIENT, PotionEffectType.FIRE_RESISTANCE, PotionEffectType.STRENGTH),
     AIR("Air", "#B0E0E6", 1004, Particle.CLOUD, Material.FEATHER, Sound.ENTITY_ENDER_DRAGON_FLAP, PotionEffectType.JUMP_BOOST, PotionEffectType.SLOW_FALLING),
     LIGHTNING("Lightning", "#FFFF00", 1005, Particle.ELECTRIC_SPARK, Material.TRIDENT, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, PotionEffectType.SPEED, PotionEffectType.NIGHT_VISION),
-    ICE("Ice", "#00FFFF", 1006, Particle.SNOWFLAKE, Material.ICE, Sound.BLOCK_GLASS_PLACE, PotionEffectType.SLOWNESS, PotionEffectType.FROST_WALKER), // Note: Resistance to Slowness is custom-handled
+    ICE("Ice", "#00FFFF", 1006, Particle.SNOWFLAKE, Material.ICE, Sound.BLOCK_GLASS_PLACE, PotionEffectType.SLOWNESS, null), // Frost Walker handled manually
     NATURE("Nature", "#228B22", 1007, Particle.HAPPY_VILLAGER, Material.OAK_SAPLING, Sound.BLOCK_GRASS_PLACE, PotionEffectType.REGENERATION, PotionEffectType.SATURATION),
     SHADOW("Shadow", "#4B0082", 1008, Particle.SMOKE, Material.WITHER_ROSE, Sound.ENTITY_GHAST_SHOOT, PotionEffectType.NIGHT_VISION, PotionEffectType.INVISIBILITY),
     LIGHT("Light", "#FFFACD", 1009, Particle.END_ROD, Material.GLOWSTONE, Sound.BLOCK_BEACON_ACTIVATE, PotionEffectType.GLOWING, PotionEffectType.ABSORPTION);
