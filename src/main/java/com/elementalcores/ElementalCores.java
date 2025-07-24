ff941962907f869cfbb46d9f3a673e908e1ea2d6
Ayanishaqq <ayanishap.gaming@gmail.com>
2025-07-24 14:46:37 +0500

    Update ElementalCores.java

--- src/main/java/com/elementalcores/ElementalCores.java
+++ src/main/java/com/elementalcores/ElementalCores.java
@@ -1,11 +1,13 @@
 package com.elementalcores;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class ElementalCores extends JavaPlugin implements Listener {  // Implements Listener for join event
+public class ElementalCores extends JavaPlugin implements Listener {
 
     private CoreManager coreManager;
     private AbilityListener abilityListener;
