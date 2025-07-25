c95c8d5239872eb33ca0847117622e96ca4ee2b2
Ayanishaqq <ayanishap.gaming@gmail.com>
2025-07-25 11:19:10 +0500

    Update RecipeManager.java

--- src/main/java/com/elementalcores/RecipeManager.java
+++ src/main/java/com/elementalcores/RecipeManager.java
@@ -3,6 +3,9 @@ package com.elementalcores;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.ChatColor;
+import java.util.Arrays;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
