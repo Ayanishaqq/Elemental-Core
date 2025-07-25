c6fd4a6d6330379e17e8910b83afa503dada772a
Ayanishaqq <ayanishap.gaming@gmail.com>
2025-07-25 11:07:38 +0500

    Update RecipeManager.java

--- src/main/java/com/elementalcores/RecipeManager.java
+++ src/main/java/com/elementalcores/RecipeManager.java
@@ -25,6 +25,12 @@ public class RecipeManager {
         registerLightCoreRecipe();
     }
     
+    public void registerUpgradeRecipes() {
+        registerTier1ToTier2Recipe();
+        registerTier2ToTier3Recipe();
+        registerElementCallerRecipe();
+    }
+    
     private void registerEarthCoreRecipe() {
         ItemStack earthCore = ((ElementalCores) plugin).createCoreItem("earth", 1);
         NamespacedKey key = new NamespacedKey(plugin, "earth_core");
@@ -136,6 +142,68 @@ public class RecipeManager {
         recipe.setIngredient('G', Material.GLOWSTONE);
         recipe.setIngredient('D', Material.DIAMOND);
         recipe.setIngredient('H', Material.HEAVY_CORE);
+        
+        plugin.getServer().addRecipe(recipe);
+    }
+    
+    private void registerTier1ToTier2Recipe() {
+        // For each core type
+        for (String coreType : new String[]{"earth", "water", "fire", "air", "lightning", "ice", "nature", "shadow", "light"}) {
+            NamespacedKey key = new NamespacedKey(plugin, coreType + "_tier1_to_tier2");
+            
+            // Create a shaped recipe for upgrading this core type
+            ShapedRecipe recipe = new ShapedRecipe(key, ((ElementalCores)plugin).createCoreItem(coreType, 2));
+            
+            recipe.shape("DTD", "NCN", "DTD");
+            recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+            recipe.setIngredient('T', Material.TOTEM_OF_UNDYING);
+            recipe.setIngredient('N', Material.NETHER_STAR);
+            recipe.setIngredient('C', Material.PAPER); // Will check for correct tier1 core in CraftItemEvent
+            
+            plugin.getServer().addRecipe(recipe);
+        }
+    }
+    
+    private void registerTier2ToTier3Recipe() {
+        // For each core type
+        for (String coreType : new String[]{"earth", "water", "fire", "air", "lightning", "ice", "nature", "shadow", "light"}) {
+            NamespacedKey key = new NamespacedKey(plugin, coreType + "_tier2_to_tier3");
+            
+            // Create a shaped recipe for upgrading this core type
+            ShapedRecipe recipe = new ShapedRecipe(key, ((ElementalCores)plugin).createCoreItem(coreType, 3));
+            
+            recipe.shape("NHN", "NCN", "NHN");
+            recipe.setIngredient('N', Material.NETHERITE_BLOCK);
+            recipe.setIngredient('H', Material.HEAVY_CORE);
+            recipe.setIngredient('C', Material.PAPER); // Will check for correct tier2 core in CraftItemEvent
+            
+            plugin.getServer().addRecipe(recipe);
+        }
+    }
+    
+    private void registerElementCallerRecipe() {
+        ItemStack elementCaller = new ItemStack(Material.HEART_OF_THE_SEA);
+        ItemMeta meta = elementCaller.getItemMeta();
+        meta.setDisplayName(ChatColor.GOLD + "Element Caller");
+        meta.setLore(Arrays.asList(
+            ChatColor.GRAY + "Right-click while holding this item",
+            ChatColor.GRAY + "to transform your current core",
+            ChatColor.GRAY + "into a random one of the same tier.",
+            "",
+            ChatColor.RED + "Consumes this item when used!"
+        ));
+        elementCaller.setItemMeta(meta);
+        
+        NamespacedKey key = new NamespacedKey(plugin, "element_caller");
+        
+        // Create the recipe
+        ShapedRecipe recipe = new ShapedRecipe(key, elementCaller);
+        
+        recipe.shape("DHD", "WCW", "DHD");
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('H', Material.HEAVY_CORE);
+        recipe.setIngredient('W', Material.WITHER_SKELETON_SKULL);
+        recipe.setIngredient('C', Material.PAPER); // Will check for any core in CraftItemEvent
         
         plugin.getServer().addRecipe(recipe);
     }
